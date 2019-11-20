e4dbb8ef573cc70474ba5d3b179a1f53abeeefc4
ofy <ofy@DESKTOP-G6OEGEB>
2019-11-20 17:27:41 +0800

    我在测试

--- src/ou/java/cn/Sq.java
+++ src/ou/java/cn/Sq.java
@@ -4,7 +4,8 @@ public class Sq {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.print("hhhÄãºÃ");
+      System.out.print("ÎÒÊÇ²âÊÔ");
+      
 	}
 
 }
